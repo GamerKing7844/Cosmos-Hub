a06210ad03e6f8ab45b493aadd9ad45a73529ef1
Nathan Fontana <n.fontana.810@gmail.com>
2026-07-18 15:02:57 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -36,27 +36,30 @@ local Window = Rayfield:CreateWindow({
       Note = "Go to the GitHub Repo for the key.", -- Use this to tell the user how to get a key
       FileName = "CosmosKey", -- It is recommended to use something unique, as other scripts using Rayfield may overwrite your key file
       SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = true, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
       Key = {"TestKey"} -- List of keys that the system will accept, can be RAW file links (pastebin, github, etc.) or simple strings ("hello", "key22")
    }
 })
 
-local Tab = Window:CreateTab("Humanoid", "user-2")
+local Tab = Window:CreateTab("Player", "user-2")
 
-local Section = Humanoid:CreateSection("Power")
+local Section = Player:CreateSection("Power")
 
-local Slider = Humanoid:CreateSlider({
-   Name = "Speed",
+local Slider = Player:CreateSlider({
+   Name = "Speed Power",
    Range = {1, 256},
    Increment = 1,
    Suffix = "studs per second",
    CurrentValue = 16,
-   Flag = "speed", -- A flag is the identifier for the configuration file; make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Flag = "Speed", -- A flag is the identifier for the configuration file; make sure every element has a different flag if you're using configuration saving to ensure no overlaps
    Callback = function(Value)
-game.Players.LocalPlayer.CharacterAdded:Connect(function(Character)
-    local Humanoid = Character:WaitForChild("Humanoid")
-    -- Apply the current value of the slider to the new character
-    Humanoid.WalkSpeed = SpeedSlider.CurrentValue
-end)
+   local localPlayer = game.Players.LocalPlayer
+       
+       if localPlayer and localPlayer.Character then
+           local humanoid = localPlayer.Character:FindFirstChildOfClass("Humanoid")
+           if humanoid then
+               humanoid.WalkSpeed = Value
+           end
+       end
    end,
 })
